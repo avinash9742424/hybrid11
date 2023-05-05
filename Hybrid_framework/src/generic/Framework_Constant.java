dc13e489cc1d157bb2dbb001c93488b2534105e7
ADMIN <ADMIN@DESKTOP-UJGAVL8>
2023-05-05 13:58:02 +0530

    hybrid

---
+++ Hybrid_framework/src/generic/Framework_Constant.java
@@ -0,0 +1,13 @@
+package generic;
+
+public interface Framework_Constant {
+	String gecko_key="webdriver.gecko.driver";
+	  String gecko_value="./softwares/geckodriver.exe";
+	  String chrome_key="webdriver.gecko.driver";
+	  String chrome_value="./softwares/geckodriver.exe";
+	  String safari_key="webdriver.gecko.driver";
+	  String safari_value="./softwares/geckodriver.exe";
+	  
+	  String URL="https://www.facebook.com";
+	  
+}
---
+++ Hybrid_framework/src/test_script/Login_Script.java
@@ -0,0 +1,14 @@
+package test_script;
+
+import org.testng.annotations.Test;
+
+import generic.Generic_Class;
+
+public class Login_Script extends Generic_Class
+{
+  @Test
+  public void test1()
+  {
+	  System.out.println("ready");
+  }
+}
